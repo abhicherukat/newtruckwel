f64539616f551973d4faddf5ddf9d4c0e44b5c92
abhi <abhideepak22@gmail.com>
2020-08-10 20:00:19 +0530

    Updates

---
+++ precache-manifest.ce3f940b6728bd079878bca8209d3091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ed39a4ba769e0d1fcb8bf4622342bb",
+    "url": "/newtruckwel/index.html"
+  },
+  {
+    "revision": "bc865ad0ca2587fadb1b",
+    "url": "/newtruckwel/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "abf94117aaa9de9ff4d4",
+    "url": "/newtruckwel/static/css/main.8b076cef.chunk.css"
+  },
+  {
+    "revision": "bc865ad0ca2587fadb1b",
+    "url": "/newtruckwel/static/js/2.3e73ea02.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/newtruckwel/static/js/2.3e73ea02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf94117aaa9de9ff4d4",
+    "url": "/newtruckwel/static/js/main.eab1e93a.chunk.js"
+  },
+  {
+    "revision": "ee47888793adffaf9f59",
+    "url": "/newtruckwel/static/js/runtime-main.51d2075f.js"
+  }
+]);
